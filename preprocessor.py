11acb71cc2b6f05cf4ad9809e1366181d6b51ec5
Warstilide49 <evindrenji01@gmail.com>
2026-01-14 09:07:20 +0100

    understanding adaptive thresholding

--- preprocessor.py
+++ preprocessor.py
@@ -13,15 +13,31 @@ else:
     # Grayscale conversion
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # BW Simple visualization
-    plt.imshow(gray, cmap='gray')
-    plt.title('Grayscale Conversion Test')
-    plt.show(block=False)
-    plt.pause(0.001)
+    # Adaptive thresholding - 
+    # Description: local thresholding, im using Gaussian method for better results on varying lighting conditions
+    # Parameters: blockSize=11, C=2
+    thres1 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+    thres2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,21,4)
+    thres3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,5,1)
+
+    # Visualization
+    titles = ['BW', 'Adaptive Thres 1', 'Adaptive Thres 2', 'Adaptive Thres 3']
+    images = [gray, thres1, thres2, thres3]
+    for i in range(4):
+        plt.subplot(1, 4, i + 1)
+        plt.imshow(images[i], cmap='gray')
+        plt.title(titles[i])
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.savefig("plot.png", dpi=300)
 
     # Raw OCR Output
     try:
-        text = pytesseract.image_to_string(gray)
+        text = pytesseract.image_to_string(thres2)
         print("--- Raw OCR Output ---")
         print(text)
         print("----------------------")
