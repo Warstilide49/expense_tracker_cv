6d66aa1ed83bc5a66101a156aa4558f61f2fd486
Warstilide49 <evindrenji01@gmail.com>
2026-01-15 08:06:19 +0100

    cleanup: converted to functions

--- preprocessor.py
+++ preprocessor.py
@@ -5,41 +5,46 @@ import matplotlib.pyplot as plt
 image_path = 'samples/normal.jpg' 
 image = cv2.imread(image_path)
 
-if image is None:
-    print(f"Error: Could not load image at {image_path}")
-else:
+# Adaptive thresholding
+# Description: local thresholding, im using Gaussian method for better results on varying lighting conditions
+# Parameters: blockSize=11, C=2
+def adaptive_threshold(img, block_size, C):
+    return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY, block_size, C)
+
+def preprocess(img):
+    if image is None:
+        print(f"Error: Could not load image at {image_path}")
+        return None
+    
     print("Image loaded successfully!")
 
     # Grayscale conversion
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Adaptive thresholding - 
-    # Description: local thresholding, im using Gaussian method for better results on varying lighting conditions
-    # Parameters: blockSize=11, C=2
-    thres1 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,11,2)
-    thres2 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,21,4)
-    thres3 = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,5,1)
+    # Blur
+    img = cv2.medianBlur(gray,5)
 
+    thres_with_blur = adaptive_threshold(img, 21, 4)
+    
     # Visualization
-    titles = ['BW', 'Adaptive Thres 1', 'Adaptive Thres 2', 'Adaptive Thres 3']
-    images = [gray, thres1, thres2, thres3]
-    for i in range(4):
-        plt.subplot(1, 4, i + 1)
-        plt.imshow(images[i], cmap='gray')
-        plt.title(titles[i])
-        plt.axis('off')
+    plt.imshow(thres_with_blur, cmap='gray')
+    plt.title("AT with Blur")
+    plt.axis('off')
 
     plt.tight_layout()
     plt.savefig("plot.png", dpi=300)
+    return thres_with_blur
 
-    # Raw OCR Output
+def print_ocr_output(img):
     try:
-        text = pytesseract.image_to_string(thres2)
+        text = pytesseract.image_to_string(img)
         print("--- Raw OCR Output ---")
         print(text)
         print("----------------------")
     except Exception as e:
         print(f"Tesseract Error: {e}")
+
+preprocessed_image = preprocess(image)
+if preprocessed_image is not None:
+    print_ocr_output(preprocessed_image)
